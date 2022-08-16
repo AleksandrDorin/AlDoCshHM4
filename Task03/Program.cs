1aa4f1f5b8e38f10cdeb6a0479ef2af3c48c6320
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-16 20:28:48 +0300

    Task03 remark

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -3,7 +3,7 @@
 int[] UserSizeArray()
 {
     Console.Write("Введите длину массива: ");
-    int size = int.Parse(Console.ReadLine());
+    int size = int.Parse(Console.ReadLine() ?? "0");
     int[] array = new int[size];
     return array;
 }
@@ -11,40 +11,32 @@ int[] UserSizeArray()
 void FillRandomArray(int[] array)
 {
     Console.Write("Введите минимальное значение массива: ");
-    int min = int.Parse(Console.ReadLine());
+    int min = int.Parse(Console.ReadLine() ?? "0");
     Console.Write("Введите максимальное значение массива: ");
-    int max = int.Parse(Console.ReadLine());
+    int max = int.Parse(Console.ReadLine() ?? "0");
 
-    var random = new Random(); 
-    for(int i = 0; i < array.Length; i++)
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
     {
-        array[i] = random.Next(min, max + 1); 
+        array[i] = random.Next(min, max + 1);
     }
 }
 void PrintArray(int[] arrayCol)
 {
     Console.WriteLine("Массив :");
-    foreach(var element in arrayCol) 
-        Console.Write($"{element} ");   
-    Console.WriteLine();                   
+    foreach (var element in arrayCol)
+        Console.Write($"{element} ");
+    Console.WriteLine();
 }
 void copyarray(int[] array)
 {
     int[] copyArray = new int[array.Length];
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         copyArray[i] = array[i];
+        Console.Write($"{copyArray[i]} ");
     }
-    int count = copyArray.Length;
-    int pos = 0;
-    while (pos < count)
-    {
-        Console.Write($"{copyArray[pos]} ");
-        pos++;
-    }
-    
 }
-
 
 int[] array = UserSizeArray();
 FillRandomArray(array);
